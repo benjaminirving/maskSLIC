b9f882eae8354911fbb4bb2bd8ca7403d4a4393a
Benjamin Irving <benjamin.irving@eng.ox.ac.uk>
2016-09-23 11:35:36 +0100

    add timing

--- run_example.py
+++ run_example.py
@@ -1,9 +1,11 @@
+from __future__ import division, absolute_import, unicode_literals, print_function
 
 import matplotlib.pyplot as plt
 
 import maskslic as seg
 from skimage.segmentation import mark_boundaries
 from skimage.io import imread
+import time
 
 img = imread('chelsea.png')
 roi = imread('chelsea_mask.png')
@@ -11,9 +13,11 @@ roi = imread('chelsea_mask.png')
 # roi = img_as_float(chelsea_mask())
 roi = roi[:, :, 3] > 0
 
+t1 = time.time()
 # Note that compactness is defined differently because a grid is not used. Lower compactness for maskSLIC is equivalent
 segments = seg.slic(img, compactness=10, seed_type='nplace', mask=roi, n_segments=12,
                     recompute_seeds=True, plot_examples=True)
+print("Time: {:.2f} s".format(time.time() - t1))
 
 plt.figure()
 plt.title('maskSLIC')
@@ -21,9 +25,10 @@ plt.imshow(mark_boundaries(img, segments))
 plt.contour(roi, contours=1, colors='red', linewidths=1)
 plt.axis('off')
 
-
+t1 = time.time()
 segments = seg.slic(img, compactness=30, seed_type='grid', n_segments=80, plot_examples=False)
 # segments[roi==0] = -1
+print("Time: {:.2f} s".format(time.time() - t1))
 
 plt.figure()
 plt.title('Conventional SLIC')
